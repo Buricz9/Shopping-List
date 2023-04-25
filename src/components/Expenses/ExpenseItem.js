496fc45cc8105cd0c89329fe1235d3788f4f0e35
Jakub Burek <buricz1906@gmail.com>
2023-04-25 12:49:37 +0200

    Add Form Inputs

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,17 +1,23 @@
+import React, { useState } from "react";
+
 import "./ExpenseItem.css";
-import "./ExpenseDate";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 const ExpenseItem = (props) => {
+  const [title, setTitle] = useState(props.title);
+
+  // let title = props.title;
+
   const clickHandler = () => {
-    console.log("Clicked!!!");
+    setTitle("Update!");
+    console.log(title);
   };
 
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
       <button onClick={clickHandler}>Change Title</button>
---
+++ src/components/NewExpense/ExpenseForm.js
@@ -0,0 +1,27 @@
+import "./ExpenseForm.css";
+
+const ExpenseForm = () => {
+  return (
+    <form>
+      <div className="new-expense__controls">
+        <div className="new-expense__control">
+          <lable>Title</lable>
+          <input type="text" />
+        </div>
+        <div className="new-expense__control">
+          <lable>Amount</lable>
+          <input type="number" min="0.01" step="0.01" />
+        </div>
+        <div className="new-expense__control">
+          <lable>Date</lable>
+          <input type="date" min="2022-01-01" max="2024-12-31" />
+        </div>
+      </div>
+      <div className="new-expense__actions">
+        <button>Add Expense</button>
+      </div>
+    </form>
+  );
+};
+
+export default ExpenseForm;
